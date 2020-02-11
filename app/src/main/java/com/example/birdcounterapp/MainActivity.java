a48faa085b4118ed41690a13718c1af705c2e384
kevinphoenix16 <kevinphoenix16@augustana.edu>
2020-02-11 11:23:47 -0600

    got button to increment values

--- app/src/main/java/com/example/birdcounterapp/MainActivity.java
+++ app/src/main/java/com/example/birdcounterapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.birdcounterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int birdOneCount = 0;
+    int birdTwoCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBirdOne(View view) {
+        birdOneCount++;//increment the birdOneCount
+        TextView text=(TextView)findViewById(R.id.textView);// resource location
+        text.setText("Votes for Bird #1: "+ birdOneCount);// view in the text
+    }
+
+    public void onClickBirdTwo(View view) {
+        birdTwoCount++;//increment the birdOneCount
+        TextView text=(TextView)findViewById(R.id.textView3);// resource location
+        text.setText("Votes for Bird #2: "+ birdTwoCount);// view in the text
     }
 }
